c5f62d44eb925d01fda9a6187be0c1d7b9038970
Jeano <cappujeano@yahoo.com>
2018-03-29 21:29:26 +0800

    - updated main activity UI

--- app/src/main/java/com/upou/jeano/uber/MainActivity.java
+++ app/src/main/java/com/upou/jeano/uber/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
 
 import com.google.firebase.auth.FirebaseAuth;
@@ -17,6 +18,8 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
 
         mDriver = (Button) findViewById(R.id.driver);
